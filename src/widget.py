06453c6ff00de80b5b941e7f45c0846d095c1972
Denis Ternovoy <denis.ternovoi@mail.ru>
2025-04-25 10:59:41 +0300

    Function get_date added

--- src/widget.py
+++ src/widget.py
@@ -16,5 +16,14 @@ def mask_account_card(bank_info: str) -> str:
     return masked_account
 
 
+def get_date(user_date: str) -> str:
+    """Функция для форматирования даты"""
+
+    formatted_date = user_date.split("-")
+
+    return f"{formatted_date[2][:2]}.{formatted_date[1]}.{formatted_date[0]}"
+
+
 if __name__ == "__main__":
     print(mask_account_card("Счет 35383033474447895560"))
+    print(get_date("2024-03-11T02:26:18.671407"))
