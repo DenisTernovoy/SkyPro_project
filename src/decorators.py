87b6c439342e9f89eb4f8ee95e72d7c9dd067fc2
Denis Ternovoy <denis.ternovoi@mail.ru>
2025-05-01 23:20:08 +0300

    Add docstring for log in decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -2,6 +2,9 @@ from typing import Any, Callable
 
 
 def log(filename: str = "") -> Callable:
+    """Декоратор с параметрами, записывающий результат работы передаваемой ему функции в консоль или файл 'filename'
+    и возвращающий результат работы этой функции"""
+
     def wrapper(function: Callable) -> Callable:
         def inner(*args: Any, **kwargs: Any) -> Any:
             name = function.__name__
