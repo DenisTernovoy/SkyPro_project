1e5870cc7a0cf14ba125f47588d7e55c5c44a2fa
Denis Ternovoy <denis.ternovoi@mail.ru>
2025-06-01 11:26:05 +0300

    Func read_excel_file added

--- src/read_data.py
+++ src/read_data.py
@@ -1,9 +1,25 @@
+from typing import Union
+
 import pandas as pd
 
 
-def read_csv_file(file_path: str) -> list[dict]:
+def read_csv_file(file_path: str) -> Union[list[dict], None]:
     """Функция для считывания финансовых операций из файла CSV, возвращающая список словарей с транзакциями"""
 
-    with open(file_path, encoding="utf-8") as file:
-        df = pd.read_csv(file)
+    try:
+        df = pd.read_csv(file_path)
         return df.to_dict(orient="records")
+    except Exception as e:
+        print(f"Произошла ошибка при попытке считать файл: {e}")
+        return None
+
+
+def read_excel_file(file_path: str) -> Union[list[dict], None]:
+    """Функция для считывания финансовых операций из файла Excel, возвращающая список словарей с транзакциями"""
+
+    try:
+        df = pd.read_excel(file_path)
+        return df.to_dict(orient="records")
+    except Exception as e:
+        print(f"Произошла ошибка при попытке считать файл: {e}")
+        return None
