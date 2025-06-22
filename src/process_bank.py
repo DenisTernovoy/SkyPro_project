520057e955c366328a4240b8f0a15d968e82f8b9
Denis Ternovoy <denis.ternovoi@mail.ru>
2025-06-22 10:22:09 +0300

    Added info about process_bank.py in README.md

--- src/process_bank.py
+++ src/process_bank.py
@@ -3,6 +3,9 @@ from collections import Counter
 
 
 def process_bank_search(data: list[dict], search: str) -> list[dict]:
+    """Функция принимает список словарей с данными о банковских операциях и строку поиска,
+    а возвращает список словарей, у которых в описании есть данная строка"""
+
     filtered_arrange = []
     for i in data:
         if re.search(search.lower(), i["description"].lower()):
@@ -12,6 +15,10 @@ def process_bank_search(data: list[dict], search: str) -> list[dict]:
 
 
 def process_bank_operations(data: list[dict], categories: list) -> dict:
+    """Функция принимает список словарей с данными о банковских операциях и список категорий операций,
+    а возвращает словарь, в котором ключи — это названия категорий,
+    а значения — это количество операций в каждой категории."""
+
     operations = []
     for i in data:
         if i["description"].lower() in list(map(str.lower, categories)):
